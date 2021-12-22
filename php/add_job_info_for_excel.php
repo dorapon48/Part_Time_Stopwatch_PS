9968053953a18df6de7f7fe71fdd0053ddea7ceb
dorapon48 <dorapon48@icloud.com>
2021-12-23 02:44:01 +0900

    sqlの文章作成

--- php/add_job_info_for_excel.php
+++ php/add_job_info_for_excel.php
@@ -15,34 +15,35 @@ $res_arrays = array();
 $res_arrays["error"] = 1;
 $res_arrays["errormessage"] = "";
 
-// POSTの入力
-$file = filter_input(INPUT_POST, "file");
-$user_id = filter_input(INPUT_POST, "user_id");
-$reader = IOFactory::createReader('Xlsx');
-$spreadsheet = $reader->load($file);
-$sheet = $spreadsheet->setActiveSheetIndex(0);
-
-$writer = IOFactory::createWriter($spreadSheet, 'Xlsx');
-$writer->save('../output/output.xlsx');
-
-// SQL文
-$sql = "";
-
 try
 {
-    /*
+    // POSTの入力
+    $file = $_FILES['excel'];
+    $user_id = $_POST['user_id'];
+
+    //excelファイルを開く
+    $reader = IOFactory::createReader('Xlsx');
+    $spreadsheet = $reader->load($file["tmp_name"]);
+    $sheet = $spreadsheet->setActiveSheetIndex(0);
+
+    // SQL文
+    $sql = "SELECT * FROM part_times WHERE user_id = :user_id AND start_time BETWEEN :ss_time AND :se_time";
+
+    $sql_year = $sheet->getCell('A1')->getValue();
+    $sql_month = $sheet->getCell('A2')->getValue();
+    $y_m = (string)$sql_year . "-" . (string)$sql_month;
+    $start_month_range = $y_m . "-01 00:00:00";
+    $end_month_range = (new DateTimeImmutable)->modify('last day of ' . $y_m)->format('Y-m-d') . " 23:59:59";
+    
+    //$res_arrays["errormessage"] = $start_month_range . "***" . $end_month_range;
+
     //prepareがsql文の準備
     //bindValueで指定した文字列に代入
     //executeでsqlを実行
-    $s_t = date('Y-m-d H:i:s', strtotime($start_time)); //dateフォーマットに合わせてstringを送る
-    $e_t = date('Y-m-d H:i:s', strtotime($end_time));
+    /*
     $stmt = $pdo->prepare($sql);
     $stmt->bindValue(":user_id", $user_id, PDO::PARAM_STR);
-    $stmt->bindValue(":start_time", $s_t, PDO::PARAM_STR);
-    $stmt->bindValue(":end_time", $e_t, PDO::PARAM_STR);
-    $stmt->bindValue(":job_id", $job_id, PDO::PARAM_STR);
-    $stmt->bindValue(":job_info", $job_info, PDO::PARAM_STR);
-    $stmt->bindValue(":others", $others, PDO::PARAM_STR);
+    
     $result = $stmt->execute();
 
     //エラー時処理
@@ -51,16 +52,19 @@ try
         echo json_encode($res_arrays);
         die();
     }
-    $res_arrays["error"] = 0;
     */
+    $res_arrays["error"] = 0;
+
+    $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+    $writer->save('../output/output.xlsx');
     //返却
     echo json_encode($res_arrays);
     die();
 
 } catch (PDOException $e) {
     // エラー処理
-    //$res_arrays["errormessage"] = "不明なエラー";
-    $res_arrays["errormessage"] = $e;
+    $res_arrays["errormessage"] = "不明なエラー";
+    //$res_arrays["errormessage"] = $e;
     echo json_encode($res_arrays);
     die();
 }
