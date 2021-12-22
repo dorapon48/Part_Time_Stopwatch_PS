a0a5e76709195fa9a4e3396eb3e8bd35cbd6bb23
dorapon48 <dorapon48@icloud.com>
2021-12-23 05:33:26 +0900

    get_part_times.phpを追加

---
+++ php/get_part_times.php
@@ -0,0 +1,62 @@
+<?php
+
+// database
+$serverName = "localhost";
+$user = "root";
+$pw = "";
+$dbname = "ps";
+$dsn = 'mysql:dbname=' . $dbname . ';host=' . $serverName . ';charset=utf8';
+$pdo = new PDO($dsn, $user, $pw);
+
+// やり取りに使う配列とエラーメッセージ等
+$res_arrays = array();
+$res_arrays["error"] = 1;
+$res_arrays["errormessage"] = "";
+
+// POSTの入力
+$user_id = filter_input(INPUT_POST, "user_id");
+
+// SQL文
+$sql = "SELECT * FROM part_times WHERE user_id = :user_id";
+
+try
+{
+    //prepareがsql文の準備
+    //bindValueで指定した文字列に代入
+    //executeでsqlを実行
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(":user_id", $user_id, PDO::PARAM_STR);
+    $result = $stmt->execute();
+
+    //エラー時処理
+    if ($result != 1) {
+        $res_arrays["errormessage"] = "接続に失敗しました。";
+        echo json_encode($res_arrays);
+        die();
+    }
+
+    $items = array();
+    while ($row = $stmt->fetch()) {
+        $items[] = array(
+            'start_time' => $row['start_time'],
+            'end_time' => $row['end_time'],
+            'job_info' => $row['job_info'],
+            'others' => $row['others']
+        );
+    }
+    $res_arrays["error"] = 0;
+
+    //返却
+    $res_arrays["data"] = $items;
+    echo json_encode($res_arrays);
+    die();
+
+} catch (PDOException $e) {
+    // エラー処理
+    //$res_arrays["errormessage"] = "不明なエラー";
+    $res_arrays["errormessage"] = $e;
+    echo json_encode($res_arrays);
+    die();
+}
+
+?>
