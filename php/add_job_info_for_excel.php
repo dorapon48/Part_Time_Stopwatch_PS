ac4b0eed8400599c24aff8507893b5209aa9e05b
dorapon48 <dorapon48@icloud.com>
2021-12-23 04:31:48 +0900

    excelへの入力部分を作成

--- php/add_job_info_for_excel.php
+++ php/add_job_info_for_excel.php
@@ -34,16 +34,14 @@ try
     $y_m = (string)$sql_year . "-" . (string)$sql_month;
     $start_month_range = $y_m . "-01 00:00:00";
     $end_month_range = (new DateTimeImmutable)->modify('last day of ' . $y_m)->format('Y-m-d') . " 23:59:59";
-    
-    //$res_arrays["errormessage"] = $start_month_range . "***" . $end_month_range;
 
     //prepareがsql文の準備
     //bindValueで指定した文字列に代入
     //executeでsqlを実行
-    /*
     $stmt = $pdo->prepare($sql);
     $stmt->bindValue(":user_id", $user_id, PDO::PARAM_STR);
-    
+    $stmt->bindValue(":ss_time", $start_month_range, PDO::PARAM_STR);
+    $stmt->bindValue(":se_time", $end_month_range, PDO::PARAM_STR);
     $result = $stmt->execute();
 
     //エラー時処理
@@ -52,9 +50,55 @@ try
         echo json_encode($res_arrays);
         die();
     }
-    */
-    $res_arrays["error"] = 0;
 
+    //整理
+    $items = array();
+    while ($row = $stmt->fetch()) {
+        $items[] = array(
+            'start_time' => $row['start_time'],
+            'end_time' => $row['end_time'],
+            'job_info' => $row['job_info']
+        );
+    }
+    //$res_arrays["errormessage"] = $items;
+
+    //excelへの入力
+    for ($i = 0; $i < count($items); $i++){
+        $s_time = new DateTime($items[$i]["start_time"]);
+        $e_time = new DateTime($items[$i]["end_time"]);
+        $j_info = $items[$i]["job_info"];
+
+        //日付のみ抜き出す
+        $day_init = 4;
+        $day = $s_time->format('d');
+        $c = (int)$day + $day_init;
+
+        //x行D列を探索
+        $value_check = $sheet->getCellByColumnAndRow(4, $c);
+        
+        if ($value_check == "0"){
+            //x行D-E列に書き込み
+            $sheet->setCellValueByColumnAndRow(4, $c, $s_time->format('H:i'));
+            $sheet->setCellValueByColumnAndRow(5, $c, $e_time->format('H:i'));
+        } else {
+            //x行H-I列に書き込み
+            $sheet->setCellValueByColumnAndRow(8, $c, $s_time->format('H:i'));
+            $sheet->setCellValueByColumnAndRow(9, $c, $e_time->format('H:i'));
+        }
+        
+        //x行R列に書き込み
+        $value_check = $sheet->getCellByColumnAndRow(18, $c);
+        if ($value_check == ""){
+            $sheet->setCellValueByColumnAndRow(18, $c, $j_info);
+        } else {
+            $sheet->setCellValueByColumnAndRow(18, $c, $value_check . "，\n" . $j_info);
+        }
+        
+        //$res_arrays["errormessage"] .= $s_time->format('H:i:s') . "-";
+    }
+
+    $res_arrays["error"] = 0;
+    //書き出し
     $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
     $writer->save('../output/output.xlsx');
     //返却
